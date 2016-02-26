3a472d6943c367fb0d19c33b8411a581ebf36899
kmani2016 <webconsultant01@gmail.com>
2016-02-25 23:50:24 -0800

    Initial commit.

---
+++ gen/com/amazonaws/demo/s3transferutility/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.amazonaws.demo.s3transferutility;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
